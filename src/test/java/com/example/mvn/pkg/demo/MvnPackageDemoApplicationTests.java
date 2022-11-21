e0b3a91a71b750b04178108fcdc3d527eb3d5a02
chulei926 <31295266+chulei926@users.noreply.github.com>
2022-11-21 08:54:27 +0800

    db init

--- src/test/java/com/example/mvn/pkg/demo/MvnPackageDemoApplicationTests.java
+++ src/test/java/com/example/mvn/pkg/demo/MvnPackageDemoApplicationTests.java
@@ -1,10 +1,23 @@
 package com.example.mvn.pkg.demo;
 
+import com.example.mvn.pkg.demo.domain.User;
+import com.example.mvn.pkg.demo.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
 @SpringBootTest
 class MvnPackageDemoApplicationTests {
+
+	@Resource
+	private UserService userService;
+
+	@Test
+	void getTest(){
+		User user = userService.get(1L);
+		System.out.println(user);
+	}
 
 	@Test
 	void contextLoads() {
